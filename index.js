a57c14231f0b61d5593f565e389c4ec2421a17c4
Lisbeth21 <la_romeroh@unicah.edu>
2020-10-15 13:33:13 -0600

    examen

---
+++ index.js
@@ -0,0 +1,6 @@
+// Importar express
+const express = require("express");
+// Importar handlebars como template engine
+const exphbs = require("express-handlebars");
+// Importar body parser que nos permite acceder al cuerpo de la petición HTTP
+const bodyParser = require("body-parser");
